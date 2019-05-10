abdae4fbb380d559efc87205279e58ad6255855d
alec-r-reynolds <alechasrito@gmail.com>
2019-05-10 15:29:58 -0600

    Updated App.js and installed button depdency

--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,20 @@
-import React from "react";
-import {Heading} from "@instructure/ui-elements";
+import React, { Component } from 'react'
 
-export default function (props) {
-  return <Heading as="h1">Basic LTI App</Heading>;
+
+import { Heading } from '@instructure/ui-elements'
+import { Button } from '@instructure/ui-buttons'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" style={{ textAlign: 'center', backgroundColor: '#f3f3f3', paddingTop: '25%' }} >
+        <Heading>Now using Instructure-UI components with default Canvas theme!</Heading>
+        <Button>Submit</Button>
+        <p>Bruh momentum</p>
+      </div>
+    )
+  }
 }
+
+export default App
