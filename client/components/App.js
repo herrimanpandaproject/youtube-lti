eecbba70e5eb6104dbe6c8b17be9372f8a40a350
Alecinvader <43455924+Alecinvader@users.noreply.github.com>
2019-05-13 10:30:08 -0600

    Comment for readability

--- client/components/App.js
+++ client/components/App.js
@@ -6,6 +6,8 @@ class App extends Component {
   apiKey = 'AIzaSyBDV4M3bIZXFCTPq3cyqQoO_EqalwJvHz0';
   constructor(props) {
     super(props);
+    // res from the search() function, upon search request is stored inside state. 
+    // The JSON object using &part=snippet plus whatever else, is stored inside. 
     this.state = {};
     this.search = this.search.bind(this);
     this.handleChange = this.handleChange.bind(this);
