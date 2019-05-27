c2776511ec2c83eeb4e1476822857d8e033cf714
Jaden Hoenes <jadhoenes@gmail.com>
2019-05-26 23:48:54 -0600

    cleanup done, might port this thing to vimeo if I have time

--- client/components/EmbedButton.js
+++ client/components/EmbedButton.js
@@ -1,27 +1,12 @@
 import React, {Component} from 'react';
 import {Menu} from '@instructure/ui-menu';
 import {Button} from '@instructure/ui-buttons';
-import { IconExpandLine } from '@instructure/ui-icons';
+import {IconExpandLine} from '@instructure/ui-icons';
 
 class EmbedButton extends Component {
   constructor(props) {
     super(props);
     this.state = {selection: [1]};
-  }
-  render() {
-    return (
-        <Menu placement="bottom" trigger={<Button variant = "primary" margin = "0 x-small 0 0" icon = {IconExpandLine}>Embed</Button>}>
-          <Menu.Group
-            label="Size"
-            selected={this.state.selection}
-            onSelect={this.handleSelect}
-          >
-            <Menu.Item value="small">Small</Menu.Item>
-            <Menu.Item value="medium">Medium</Menu.Item>
-            <Menu.Item value="large">Large</Menu.Item>
-          </Menu.Group>
-        </Menu>
-    );
   }
 
   handleSelect = (e, newSelected) => {
@@ -51,6 +36,22 @@ class EmbedButton extends Component {
       src: `https://www.youtube.com/embed/${this.props.videoId}`,
       title: this.props.title,
     });
+  }
+
+  render() {
+    return (
+        <Menu placement = "bottom" trigger = {<Button variant = "primary" margin = "0 x-small 0 0" icon = {IconExpandLine}>Embed</Button>}>
+          <Menu.Group
+            label = "Size"
+            selected = {this.state.selection}
+            onSelect = {this.handleSelect}
+          >
+            <Menu.Item value = "small">Small</Menu.Item>
+            <Menu.Item value = "medium">Medium</Menu.Item>
+            <Menu.Item value = "large">Large</Menu.Item>
+          </Menu.Group>
+        </Menu>
+    );
   }
 }
 
--- client/components/VideoStats.js
+++ client/components/VideoStats.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Flex, FlexItem} from '@instructure/ui-layout'
-import numeral from 'numeral';
 import {IconEyeLine} from '@instructure/ui-icons';
 import {IconLikeLine} from '@instructure/ui-icons';
+import numeral from 'numeral';
 
-class VideoStats extends Component {
+class VideoStats extends Component { 
   styleRed = {
     width: Math.abs(this.props.percent-100)+"px",
     backgroundColor: 'red',
@@ -14,21 +14,28 @@ class VideoStats extends Component {
     width: this.props.percent+"px",
     backgroundColor: 'green',
   }
+
+  likeBarContainer = {
+    display: 'inline-flex', 
+    height: '7.5px', 
+    position: 'relative', 
+    top: '5px'
+  }
   
   render() {
     return(
-      <div style={{maxWidth: '290px'}}>
-        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+      <div style = {{maxWidth: '290px'}}>
+        <div style = {{display: 'flex', justifyContent: 'space-between'}}>
           {numeral(this.props.stats.viewCount).format('0.0a')}
-          <IconEyeLine color="primary" />
+          <IconEyeLine color = "primary" />
           {numeral(this.props.stats.likeCount).format('0.0a')}
-          <IconLikeLine color="primary"/>
-          <div style={{display: 'inline-flex', height: '7.5px', position: 'relative', top: '5px'}}>
-            <div style={this.styleGreen}></div>
-            <div style={this.styleRed}></div>
+          <IconLikeLine color = "primary"/>
+          <div style = {this.likeBarContainer}>
+            <div style = {this.styleGreen}></div>
+            <div style = {this.styleRed}></div>
           </div>
-          <IconLikeLine color="primary" rotate="180"/>
-          {numeral(this.props.stats.dislikeCunt).format('0.0a')}
+          <IconLikeLine color = "primary" rotate ="180"/>
+          {numeral(this.props.stats.dislikeCount).format('0.0a')}
         </div>
       </div>
     )
